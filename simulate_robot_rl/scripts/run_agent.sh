7d83bb5f3864fc35d5d9f5395861690e59ccbea9
kuwabaray <yukiya104@gmail.com>
2021-02-11 08:21:19 +0000

    add simulate worker

--- simulate_robot_rl/scripts/run_agent.sh
+++ simulate_robot_rl/scripts/run_agent.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
-export PYTHONPATH="$PYTHONPATH:/home/yukiya/catkin_ws/src/dqn_for_slam"
+
+dir_path=$(cd $(dirname $0);pwd)
+pkg_name='dqn_for_slam'
+pkg_path=${dir_path::-25}$pkg_name
+export PYTHONPATH="$PYTHONPATH:$pkg_path"
 
 python3 -m dqn_for_slam.rl_worker
